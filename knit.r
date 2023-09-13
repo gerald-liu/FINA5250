d4eb4946923f7ac6435875b57cf2e791b751865f
gerald-liu <wliuax@connect.ust.hk>
2023-09-13 17:43:04 +0800

    assignment 2

---
+++ knit.r
@@ -0,0 +1,9 @@
+assg_no <- 2
+wd <- sprintf("D:/workspace/FINA5250/Assignment%s", assg_no)
+
+knitr::purl(input = sprintf("%s/assg%s.rmd", wd, assg_no),
+            output = sprintf("%s/assg%s.r", wd, assg_no),
+            documentation = 0)
+
+file.rename(from = sprintf("%s/assg%s.r", wd, assg_no),
+            to = sprintf("%s/assg%s.txt", wd, assg_no))
